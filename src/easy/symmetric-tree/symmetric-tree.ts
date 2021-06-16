fc79d2e90632d766d28a3d2d0809d7a6dd1bf9a3
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-16 16:27:35 +0800

    refactor: tree traversal in symmetric-tree

--- src/easy/symmetric-tree/symmetric-tree.ts
+++ src/easy/symmetric-tree/symmetric-tree.ts
@@ -1,21 +1,15 @@
 import { TreeNode } from "lc/tools/tree-node";
 
-function visit(l: TreeNode | null, r: TreeNode | null): boolean {
-    if (l === null) {
-        return r === null;
+function isMirror(l: TreeNode | null, r: TreeNode | null): boolean {
+    if (l === null && r === null) {
+        return true;
     }
-    if (r === null) {
+    if (l === null || r === null) {
         return false;
     }
-    if (l.val !== r.val) {
-        return false;
-    }
-    return visit(l.right, r.left) && visit(l.left, r.right);
+    return l.val === r.val && isMirror(l.right, r.left) && isMirror(l.left, r.right);
 }
 
 export function isSymmetric(root: TreeNode | null): boolean {
-    if (root === null) {
-        return true;
-    }
-    return visit(root.left, root.right);
+    return isMirror(root, root);
 }
