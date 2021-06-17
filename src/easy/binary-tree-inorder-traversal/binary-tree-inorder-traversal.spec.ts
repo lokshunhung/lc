7f0d1ce9cea2589adce1a114f95912da0d70f238
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-17 16:25:35 +0800

    refactor: fix incorrect tree-node fixture

--- src/easy/binary-tree-inorder-traversal/binary-tree-inorder-traversal.spec.ts
+++ src/easy/binary-tree-inorder-traversal/binary-tree-inorder-traversal.spec.ts
@@ -2,12 +2,12 @@ import { toTreeNode } from "lc/tools/tree-node";
 import { inorderTraversal } from "./binary-tree-inorder-traversal";
 
 test.each`
-    root                           | expectedOutput
-    ${[1, null, 2, null, null, 3]} | ${[1, 3, 2]}
-    ${[]}                          | ${[]}
-    ${[1]}                         | ${[1]}
-    ${[1, 2]}                      | ${[2, 1]}
-    ${[1, null, 2]}                | ${[1, 2]}
+    root               | expectedOutput
+    ${[1, null, 2, 3]} | ${[1, 3, 2]}
+    ${[]}              | ${[]}
+    ${[1]}             | ${[1]}
+    ${[1, 2]}          | ${[2, 1]}
+    ${[1, null, 2]}    | ${[1, 2]}
 `("_($root) -> $expectedOutput", ({ root, expectedOutput }: any) => {
     let tRoot = toTreeNode(root);
     let result = inorderTraversal(tRoot);
