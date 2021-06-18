566d2f3894485158d2294b9e65eb9ba96d2a3472
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-18 09:57:34 +0800

    refactor: dfs (_)

--- src/_/dfs/dfs.ts
+++ src/_/dfs/dfs.ts
@@ -1,10 +1,17 @@
 import { TreeNode } from "lc/tools/tree-node";
 
+function forEachChildren(treeNode: TreeNode, visitChild: (childNode: TreeNode) => void): void {
+    [treeNode.left, treeNode.right]
+        .filter((child: TreeNode | null): child is TreeNode => child !== null)
+        .forEach(visitChild);
+}
+
 export function DFS(root: TreeNode | null, visit: (treeNode: TreeNode) => void): void {
     if (root === null) {
         return;
     }
     visit(root);
-    DFS(root.left, visit);
-    DFS(root.right, visit);
+    forEachChildren(root, (childNode) => {
+        DFS(childNode, visit);
+    });
 }
