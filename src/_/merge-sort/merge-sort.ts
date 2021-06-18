ca4ceba2d08da5bf3ef96b29884632b38b42452b
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-18 11:54:36 +0800

    feat: merge-sort (_) in place

--- src/_/merge-sort/merge-sort.ts
+++ src/_/merge-sort/merge-sort.ts
@@ -1,33 +1,37 @@
-function merge(a: Array<number>, b: Array<number>): Array<number> {
-    let result: Array<number> = [];
+function merge(arr: Array<number>, lo: number, mid: number, hi: number): void {
+    let a = arr.slice(lo, mid + 1);
+    let b = arr.slice(mid + 1, hi + 1);
     let i = 0;
     let j = 0;
+    let k = lo;
     while (i < a.length && j < b.length) {
         if (a[i] < b[j]) {
-            result.push(a[i]);
+            arr[k] = a[i];
             ++i;
+            ++k;
         } else {
-            result.push(b[j]);
+            arr[k] = b[j];
             ++j;
+            ++k;
         }
     }
     while (i < a.length) {
-        result.push(a[i]);
+        arr[k] = a[i];
         ++i;
+        ++k;
     }
     while (j < b.length) {
-        result.push(b[j]);
+        arr[k] = b[j];
         ++j;
+        ++k;
     }
-    return result;
 }
 
-export function mergeSort(arr: Array<number>): Array<number> {
-    if (arr.length <= 1) {
-        return arr;
+export function mergeSort(arr: Array<number>, lo: number = 0, hi: number = arr.length - 1): void {
+    if (lo < hi) {
+        let mid = ~~((lo + hi) / 2);
+        mergeSort(arr, lo, mid);
+        mergeSort(arr, mid + 1, hi);
+        merge(arr, lo, mid, hi);
     }
-    let mid = ~~(arr.length / 2);
-    let a = mergeSort(arr.slice(0, mid));
-    let b = mergeSort(arr.slice(mid, arr.length));
-    return merge(a, b);
 }
