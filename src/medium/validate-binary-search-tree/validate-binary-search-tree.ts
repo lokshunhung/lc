db409017af96ef79e31aed694045fa38d1f8c178
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-21 10:36:03 +0800

    refactor: validate-binary-search-tree

--- src/medium/validate-binary-search-tree/validate-binary-search-tree.ts
+++ src/medium/validate-binary-search-tree/validate-binary-search-tree.ts
@@ -1,34 +1,17 @@
 import { TreeNode } from "lc/tools/tree-node";
 
-interface Result {
-    min: number | null;
-    max: number | null;
-}
-
-function visitTreeNode(root: TreeNode | null, result: Result): boolean {
-    if (root === null) {
+function visitTreeNode(treeNode: TreeNode | null, minNode: TreeNode | null, maxNode: TreeNode | null): boolean {
+    if (treeNode === null) {
         return true;
     }
-    let leftRes: Result = { min: null, max: null };
-    let rightRes: Result = { min: null, max: null };
-    let isLeftValid = visitTreeNode(root.left, leftRes);
-    let isRightValid = visitTreeNode(root.right, rightRes);
-    let isSelfValid =
-        (leftRes.max ?? Number.MIN_SAFE_INTEGER) < root.val && (rightRes.min ?? Number.MAX_SAFE_INTEGER) > root.val;
-    if (!(isLeftValid && isRightValid && isSelfValid)) {
-        return false;
+    if ((minNode === null || minNode.val < treeNode.val) && (maxNode === null || treeNode.val < maxNode.val)) {
+        let isLeftValid = visitTreeNode(treeNode.left, minNode, treeNode);
+        let isRightValid = visitTreeNode(treeNode.right, treeNode, maxNode);
+        return isLeftValid && isRightValid;
     }
-    result.min = Math.min.apply(
-        null,
-        [leftRes.min, root.val, rightRes.min].filter((_): _ is number => _ !== null)
-    );
-    result.max = Math.max.apply(
-        null,
-        [leftRes.max, root.val, rightRes.max].filter((_): _ is number => _ !== null)
-    );
-    return true;
+    return false;
 }
 
 export function isValidBST(root: TreeNode | null): boolean {
-    return visitTreeNode(root, { min: null, max: null });
+    return visitTreeNode(root, null, null);
 }
