50d261cbca034ccc42e44d3d367bd07347bb3a04
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-06-23 17:26:31 +0800

    refactor: clone-graph (medium)

--- src/medium/clone-graph/clone-graph.ts
+++ src/medium/clone-graph/clone-graph.ts
@@ -7,16 +7,26 @@ class Node {
     }
 }
 
-function register(reg: Record<number, number[]>, node: Node): void {
-    let val = node.val;
-    reg[val] = [];
-    for (let n of node.neighbors) {
-        reg[val].push(n.val);
+function visitNode(seen: Record<string, 1>, clonedNodes: Record<string, Node>, originalNode: Node): void {
+    let val = originalNode.val;
+    if (val in seen) {
+        return;
     }
-    for (let n of node.neighbors) {
-        if (!(n.val in reg)) {
-            register(reg, n);
+    seen[val] = 1;
+    if (!(val in clonedNodes)) {
+        clonedNodes[val] = new Node(val);
+    }
+    let clonedNode = clonedNodes[val];
+    for (let n of originalNode.neighbors) {
+        let neighborVal = n.val;
+        if (!(neighborVal in clonedNodes)) {
+            clonedNodes[neighborVal] = new Node(neighborVal);
         }
+        let clonedNeighbor = clonedNodes[neighborVal];
+        clonedNode.neighbors.push(clonedNeighbor);
+    }
+    for (let n of originalNode.neighbors) {
+        visitNode(seen, clonedNodes, n);
     }
 }
 
@@ -24,21 +34,8 @@ export function cloneGraph(node: Node | null): Node | null {
     if (node === null) {
         return null;
     }
-
-    let reg: Record<string, number[]> = {};
-    register(reg, node);
-
-    let nodes: Record<string, Node> = {};
-    for (let k of Object.keys(reg)) {
-        let nk = Number(k);
-        nodes[nk] = new Node(nk);
-    }
-    for (let k of Object.keys(reg)) {
-        let neighbors = reg[k];
-        for (let n of neighbors) {
-            nodes[k].neighbors.push(nodes[n]);
-        }
-    }
-
-    return nodes[node.val];
+    let seen: Record<string, 1> = {};
+    let clonedNodes: Record<string, Node> = {};
+    visitNode(seen, clonedNodes, node);
+    return clonedNodes[node.val];
 }
